5f053ff0ad4d40853275c0410a67f8693fc6e97f
Sam Harwell <sam.harwell@rackspace.com>
2013-08-14 11:39:19 -0500

    Private set accessors for Role, UserCredential

--- src/corelib/Core/Domain/UserCredential.cs
+++ src/corelib/Core/Domain/UserCredential.cs
@@ -6,12 +6,19 @@ namespace net.openstack.Core.Domain
     public class UserCredential
     {
         [DataMember]
-        public string Name { get; set; }
+        public string Name { get; private set; }
 
         [DataMember(Name = "username")]
-        public string Username { get; set; }
+        public string Username { get; private set; }
 
         [DataMember(Name = "apiKey")]
-        public string APIKey { get; set; }
+        public string APIKey { get; private set; }
+
+        public UserCredential(string name, string username, string apiKey)
+        {
+            Name = name;
+            Username = username;
+            APIKey = apiKey;
+        }
     }
 }
