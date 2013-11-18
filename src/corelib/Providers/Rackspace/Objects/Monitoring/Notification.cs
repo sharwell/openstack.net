8c1d511999743f5c714960a75999a8448922094f
Sam Harwell <sam.harwell@rackspace.com>
2013-11-18 12:23:32 -0600

    Support the created_at, updated_at, and metadata properties for notifications

--- src/corelib/Providers/Rackspace/Objects/Monitoring/Notification.cs
+++ src/corelib/Providers/Rackspace/Objects/Monitoring/Notification.cs
@@ -1,13 +1,29 @@
 ﻿namespace net.openstack.Providers.Rackspace.Objects.Monitoring
 {
+    using System;
     using Newtonsoft.Json;
 
     [JsonObject(MemberSerialization.OptIn)]
     public class Notification : NotificationConfiguration
     {
 #pragma warning disable 649 // Field 'fieldName' is never assigned to, and will always have its default value {value}
+        /// <summary>
+        /// This is the backing field for the <see cref="Id"/> property.
+        /// </summary>
         [JsonProperty("id")]
         private NotificationId _id;
+
+        /// <summary>
+        /// This is the backing field for the <see cref="Created"/> property.
+        /// </summary>
+        [JsonProperty("created_at")]
+        private long? _createdAt;
+
+        /// <summary>
+        /// This is the backing field for the <see cref="LastModified"/> property.
+        /// </summary>
+        [JsonProperty("updated_at")]
+        private long? _updatedAt;
 #pragma warning restore 649
 
         /// <summary>
@@ -19,11 +35,36 @@
         {
         }
 
+        /// <summary>
+        /// Gets the unique identifier for the notification resource.
+        /// </summary>
         public NotificationId Id
         {
             get
             {
                 return _id;
+            }
+        }
+
+        /// <summary>
+        /// Gets a timestamp indicating when the notification was first created.
+        /// </summary>
+        public DateTimeOffset? Created
+        {
+            get
+            {
+                return DateTimeOffsetExtensions.ToDateTimeOffset(_createdAt);
+            }
+        }
+
+        /// <summary>
+        /// Gets a timestamp indicating when the notification was last modified.
+        /// </summary>
+        public DateTimeOffset? LastModified
+        {
+            get
+            {
+                return DateTimeOffsetExtensions.ToDateTimeOffset(_updatedAt);
             }
         }
     }
--- src/corelib/Providers/Rackspace/Objects/Monitoring/NotificationConfiguration.cs
+++ src/corelib/Providers/Rackspace/Objects/Monitoring/NotificationConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿namespace net.openstack.Providers.Rackspace.Objects.Monitoring
 {
     using System;
+    using System.Collections.Generic;
+    using net.openstack.Core.Collections;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
 
@@ -17,6 +19,12 @@
         private JObject _details;
 
         /// <summary>
+        /// This is the backing field for the <see cref="Metadata"/> property.
+        /// </summary>
+        [JsonProperty("metadata", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        private IDictionary<string, string> _metadata;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="NotificationConfiguration"/> class
         /// during JSON deserialization.
         /// </summary>
@@ -25,7 +33,7 @@
         {
         }
 
-        public NotificationConfiguration(string label, NotificationTypeId notificationTypeId, NotificationDetails details)
+        public NotificationConfiguration(string label, NotificationTypeId notificationTypeId, NotificationDetails details, IDictionary<string, string> metadata = null)
         {
             _label = label;
             _type = notificationTypeId;
@@ -36,6 +44,8 @@
 
                 _details = JObject.FromObject(details);
             }
+
+            _metadata = metadata;
         }
 
         public string Label
@@ -59,6 +69,20 @@
             get
             {
                 return NotificationDetails.FromJObject(Type, _details);
+            }
+        }
+
+        /// <summary>
+        /// Gets a collection of metadata associated with the notification plan.
+        /// </summary>
+        public ReadOnlyDictionary<string, string> Metadata
+        {
+            get
+            {
+                if (_metadata == null)
+                    return null;
+
+                return new ReadOnlyDictionary<string, string>(_metadata);
             }
         }
     }
