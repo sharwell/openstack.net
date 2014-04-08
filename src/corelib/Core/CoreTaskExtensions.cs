c2a0ea517ac3619ef86de186a32c1007213c4853
Sam Harwell <sam.harwell@rackspace.com>
2014-04-08 12:42:11 -0500

    Add the CoreTaskExtensions.Finally methods

--- src/corelib/Core/CoreTaskExtensions.cs
+++ src/corelib/Core/CoreTaskExtensions.cs
@@ -342,6 +342,214 @@
         }
 
         /// <summary>
+        /// Synchronously execute a cleanup continuation when a task completes, regardless of the
+        /// final <see cref="Task.Status"/> of the task. If the cleanup action completes
+        /// successfully, the status of the antecedent is directly applied to the task returned by
+        /// this method. Otherwise, the status of the faulted or canceled cleanup operation is
+        /// directly applied to the task returned by this method.
+        /// </summary>
+        /// <remarks>
+        /// <para>This method ensures that exception information provided by a faulted or canceled
+        /// task is not wrapped in an additional <see cref="AggregateException"/>.
+        /// </para>
+        ///
+        /// <note type="caller">
+        /// Since the continuation is executed synchronously, this method should only be used for
+        /// lightweight continuation functions. For non-trivial continuation functions, use a
+        /// <see cref="Task"/> for the continuation operation and call
+        /// <see cref="Finally(Task, Func{Task, Task})"/> instead.
+        /// </note>
+        /// </remarks>
+        /// <param name="task">The antecedent task.</param>
+        /// <param name="cleanupAction">The cleanup continuation function to execute when <paramref name="task"/> completes.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="task"/> is <see langword="null"/>.
+        /// <para>-or-</para>
+        /// <para>If <paramref name="cleanupAction"/> is <see langword="null"/>.</para>
+        /// </exception>
+        public static Task Finally(this Task task, Action<Task> cleanupAction)
+        {
+            if (task == null)
+                throw new ArgumentNullException("task");
+            if (cleanupAction == null)
+                throw new ArgumentNullException("cleanupAction");
+
+            TaskCompletionSource<VoidResult> completionSource = new TaskCompletionSource<VoidResult>();
+
+            task
+                .ContinueWith(cleanupAction, TaskContinuationOptions.ExecuteSynchronously)
+                .ContinueWith(
+                    t =>
+                    {
+                        if (t.Status == TaskStatus.RanToCompletion)
+                            completionSource.SetFromTask(task);
+                        else
+                            completionSource.SetFromTask(t);
+                    }, TaskContinuationOptions.ExecuteSynchronously);
+
+            return completionSource.Task;
+        }
+
+        /// <summary>
+        /// Synchronously execute a cleanup continuation when a task completes, regardless of the
+        /// final <see cref="Task.Status"/> of the task. If the cleanup action completes
+        /// successfully, the status of the antecedent is directly applied to the task returned by
+        /// this method. Otherwise, the status of the faulted or canceled cleanup operation is
+        /// directly applied to the task returned by this method.
+        /// </summary>
+        /// <remarks>
+        /// <para>This method ensures that exception information provided by a faulted or canceled
+        /// task is not wrapped in an additional <see cref="AggregateException"/>.
+        /// </para>
+        ///
+        /// <note type="caller">
+        /// Since the continuation is executed synchronously, this method should only be used for
+        /// lightweight continuation functions. For non-trivial continuation functions, use a
+        /// <see cref="Task"/> for the continuation operation and call
+        /// <see cref="Finally(Task, Func{Task, Task})"/> instead.
+        /// </note>
+        /// </remarks>
+        /// <typeparam name="TResult">The type of the result produced by the antecedent <see cref="Task{TResult}"/>.</typeparam>
+        /// <param name="task">The antecedent task.</param>
+        /// <param name="cleanupAction">The cleanup continuation function to execute when <paramref name="task"/> completes.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation. When the task
+        /// completes successfully, the <see cref="Task{TResult}.Result"/> property will return
+        /// the result of the antecedent <paramref name="task"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="task"/> is <see langword="null"/>.
+        /// <para>-or-</para>
+        /// <para>If <paramref name="cleanupAction"/> is <see langword="null"/>.</para>
+        /// </exception>
+        public static Task<TResult> Finally<TResult>(this Task<TResult> task, Action<Task<TResult>> cleanupAction)
+        {
+            if (task == null)
+                throw new ArgumentNullException("task");
+            if (cleanupAction == null)
+                throw new ArgumentNullException("cleanupAction");
+
+            TaskCompletionSource<TResult> completionSource = new TaskCompletionSource<TResult>();
+
+            task
+                .ContinueWith(cleanupAction, TaskContinuationOptions.ExecuteSynchronously)
+                .ContinueWith(
+                    t =>
+                    {
+                        if (t.Status == TaskStatus.RanToCompletion)
+                            completionSource.SetFromTask(task);
+                        else
+                            completionSource.SetFromTask(t);
+                    }, TaskContinuationOptions.ExecuteSynchronously);
+
+            return completionSource.Task;
+        }
+
+        /// <summary>
+        /// Execute a cleanup continuation task when a task completes, regardless of the final
+        /// <see cref="Task.Status"/> of the antecedent task. If the cleanup action completes
+        /// successfully, the status of the antecedent is directly applied to the task returned by
+        /// this method. Otherwise, the status of the faulted or canceled cleanup operation is
+        /// directly applied to the task returned by this method.
+        /// </summary>
+        /// <remarks>
+        /// <para>This method ensures that exception information provided by a faulted or canceled
+        /// task is not wrapped in an additional <see cref="AggregateException"/>.
+        /// </para>
+        ///
+        /// <note type="caller">
+        /// Since the <paramref name="cleanupFunction"/> is executed synchronously, this
+        /// method should only be used for lightweight continuation functions. This restriction
+        /// applies only to <paramref name="cleanupFunction"/> itself, not to the
+        /// <see cref="Task"/> returned by it.
+        /// </note>
+        /// </remarks>
+        /// <param name="task">The antecedent task.</param>
+        /// <param name="cleanupFunction">The continuation function to execute when <paramref name="task"/> completes. The continuation function returns a <see cref="Task"/> representing the asynchronous cleanup operation.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="task"/> is <see langword="null"/>.
+        /// <para>-or-</para>
+        /// <para>If <paramref name="cleanupFunction"/> is <see langword="null"/>.</para>
+        /// </exception>
+        public static Task Finally(this Task task, Func<Task, Task> cleanupFunction)
+        {
+            if (task == null)
+                throw new ArgumentNullException("task");
+            if (cleanupFunction == null)
+                throw new ArgumentNullException("cleanupFunction");
+
+            TaskCompletionSource<VoidResult> completionSource = new TaskCompletionSource<VoidResult>();
+
+            task
+                .ContinueWith(cleanupFunction, TaskContinuationOptions.ExecuteSynchronously)
+                .Unwrap()
+                .ContinueWith(
+                    t =>
+                    {
+                        if (t.Status == TaskStatus.RanToCompletion)
+                            completionSource.SetFromTask(task);
+                        else
+                            completionSource.SetFromTask(t);
+                    }, TaskContinuationOptions.ExecuteSynchronously);
+
+            return completionSource.Task;
+        }
+
+        /// <summary>
+        /// Execute a cleanup continuation task when a task completes, regardless of the final
+        /// <see cref="Task.Status"/> of the antecedent task. If the cleanup action completes
+        /// successfully, the status of the antecedent is directly applied to the task returned by
+        /// this method. Otherwise, the status of the faulted or canceled cleanup operation is
+        /// directly applied to the task returned by this method.
+        /// </summary>
+        /// <remarks>
+        /// <para>This method ensures that exception information provided by a faulted or canceled
+        /// task is not wrapped in an additional <see cref="AggregateException"/>.
+        /// </para>
+        ///
+        /// <note type="caller">
+        /// Since the <paramref name="cleanupFunction"/> is executed synchronously, this
+        /// method should only be used for lightweight continuation functions. This restriction
+        /// applies only to <paramref name="cleanupFunction"/> itself, not to the
+        /// <see cref="Task"/> returned by it.
+        /// </note>
+        /// </remarks>
+        /// <typeparam name="TResult">The type of the result produced by the antecedent <see cref="Task{TResult}"/>.</typeparam>
+        /// <param name="task">The antecedent task.</param>
+        /// <param name="cleanupFunction">The continuation function to execute when <paramref name="task"/> completes. The continuation function returns a <see cref="Task"/> representing the asynchronous cleanup operation.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation. When the task
+        /// completes successfully, the <see cref="Task{TResult}.Result"/> property will return
+        /// the result of the antecedent <paramref name="task"/>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="task"/> is <see langword="null"/>.
+        /// <para>-or-</para>
+        /// <para>If <paramref name="cleanupFunction"/> is <see langword="null"/>.</para>
+        /// </exception>
+        public static Task<TResult> Finally<TResult>(this Task<TResult> task, Func<Task<TResult>, Task> cleanupFunction)
+        {
+            if (task == null)
+                throw new ArgumentNullException("task");
+            if (cleanupFunction == null)
+                throw new ArgumentNullException("cleanupFunction");
+
+            TaskCompletionSource<TResult> completionSource = new TaskCompletionSource<TResult>();
+
+            task
+                .ContinueWith(cleanupFunction, TaskContinuationOptions.ExecuteSynchronously)
+                .Unwrap()
+                .ContinueWith(
+                    t =>
+                    {
+                        if (t.Status == TaskStatus.RanToCompletion)
+                            completionSource.SetFromTask(task);
+                        else
+                            completionSource.SetFromTask(t);
+                    }, TaskContinuationOptions.ExecuteSynchronously);
+
+            return completionSource.Task;
+        }
+
+        /// <summary>
         /// Execute a continuation task when a task completes successfully. The continuation
         /// task is synchronously created by a continuation function, and then unwrapped to
         /// form the result of this method.
