714e3d75c72f87f4e690e3f4d5e81b1f56d4e1fb
Sam Harwell <sam.harwell@rackspace.com>
2014-04-11 15:38:34 -0500

    Add StringBuilderExtensions to provide the Clear method in .NET 3.5

---
+++ src/corelib/Core/Compat/StringBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace net.openstack.Core.Compat
+{
+    using System;
+    using System.Text;
+
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder Clear(this StringBuilder builder)
+        {
+            if (builder == null)
+                throw new ArgumentNullException("builder");
+
+            builder.Length = 0;
+            return builder;
+        }
+    }
+}
