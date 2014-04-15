b86f74da8ee90d0639f8779b1c3721ab69fa0631
Sam Harwell <sam.harwell@rackspace.com>
2014-04-15 08:33:25 -0500

    Updates to address XML documentation build warnings in .NET 4.0 build

--- src/corelib/Core/Compat/CustomAttributeExtensions.cs
+++ src/corelib/Core/Compat/CustomAttributeExtensions.cs
@@ -2,14 +2,25 @@
 {
     using System.Linq;
 
+    /// <summary>
+    /// Contains static methods for retrieving custom attributes.
+    /// </summary>
     internal static class CustomAttributeExtensions
     {
-        public static T GetCustomAttribute<T>(this Assembly assembly)
+        /// <summary>
+        /// Retrieves a custom attribute of a specified type that is applied to a specified assembly.
+        /// </summary>
+        /// <typeparam name="T">The type of attribute to search for.</typeparam>
+        /// <param name="element">The assembly to inspect.</param>
+        /// <returns>A custom attribute that matches <typeparamref name="T"/>, or <see langword="null"/> if no such attribute is found.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="element"/> is <see langword="null"/>.</exception>
+        public static T GetCustomAttribute<T>(this Assembly element)
+            where T : Attribute
         {
-            if (assembly == null)
-                throw new ArgumentNullException("assembly");
+            if (element == null)
+                throw new ArgumentNullException("element");
 
-            return assembly.GetCustomAttributes(typeof(T), false).OfType<T>().First();
+            return element.GetCustomAttributes(typeof(T), false).OfType<T>().FirstOrDefault();
         }
     }
 }
