95a67029e0ec56d11c2bfee0ef5ed5782e6e8c58
Sam Harwell <sam.harwell@rackspace.com>
2014-04-24 15:03:28 -0500

    Added the IAuthenticationService interface and an initial ServiceClient base class and QueuesClient service client

---
+++ src/corelib/OpenStack/Security/Authentication/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenStack.Authentication
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IAuthenticationService
+    {
+        Task<Uri> GetBaseAddressAsync(string serviceType, string serviceName, string region, bool internalAddress, CancellationToken cancellationToken);
+
+        Task AuthenticateRequestAsync(HttpRequestMessage requestMessage, CancellationToken cancellationToken);
+    }
+}
