c7a1ff37b2400d742a2840a7cb96c29db05abf7a
Sam Harwell <sam.harwell@rackspace.com>
2014-04-26 17:40:54 -0500

    Implement support methods for creating instances of HttpApiCall<T>

---
+++ src/corelib/OpenStack/Net/HttpApiCall.cs
@@ -0,0 +1,53 @@
+﻿namespace OpenStack.Net
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Rackspace.Threading;
+
+#if !NET40PLUS
+    using OpenStack.Compat;
+#endif
+
+    public class HttpApiCall : HttpApiCall<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpApiCall"/> class
+        /// with the specified request message.
+        /// </summary>
+        /// <param name="requestMessage">The <see cref="HttpRequestMessage"/> representing the HTTP API request.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="requestMessage"/> is <see langword="null"/>.</exception>
+        public HttpApiCall(HttpClient httpClient, HttpRequestMessage requestMessage, HttpCompletionOption completionOption)
+            : base(httpClient, requestMessage, completionOption)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpApiCall"/> class
+        /// with the specified request message.
+        /// </summary>
+        /// <param name="requestMessage">The <see cref="HttpRequestMessage"/> representing the HTTP API request.</param>
+        /// <param name="disposeMessage"><see langword="true"/> to call <see cref="IDisposable.Dispose"/> on the <paramref name="requestMessage"/> object when this object is disposed; otherwise, <see langword="false"/>. The default value is <see langword="true"/>.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="requestMessage"/> is <see langword="null"/>.</exception>
+        public HttpApiCall(HttpClient httpClient, HttpRequestMessage requestMessage, HttpCompletionOption completionOption, bool disposeMessage)
+            : base(httpClient, requestMessage, completionOption, disposeMessage)
+        {
+        }
+
+        public override Task<Tuple<HttpResponseMessage, string>> SendAsync(CancellationToken cancellationToken)
+        {
+            return HttpClient.SendAsync(RequestMessage, CompletionOption, cancellationToken)
+                .Then(
+                    task =>
+                    {
+                        if (task.Result.Content == null)
+                            return CompletedTask.FromResult(Tuple.Create(task.Result, default(string)));
+
+                        return task.Result.Content.ReadAsStringAsync()
+                            .Select(innerTask => Tuple.Create(task.Result, innerTask.Result));
+                    });
+        }
+    }
+}
---
+++ src/corelib/OpenStack/Net/JsonHttpApiCall`1.cs
@@ -0,0 +1,43 @@
+﻿namespace OpenStack.Net
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using Rackspace.Threading;
+
+#if !NET40PLUS
+    using OpenStack.Compat;
+#endif
+
+    public class JsonHttpApiCall<T> : HttpApiCall<T>
+    {
+        public JsonHttpApiCall(HttpClient httpClient, HttpRequestMessage requestMessage, HttpCompletionOption completionOption)
+            : base(httpClient, requestMessage, completionOption)
+        {
+        }
+
+        public JsonHttpApiCall(HttpClient httpClient, HttpRequestMessage requestMessage, HttpCompletionOption completionOption, bool disposeMessage)
+            : base(httpClient, requestMessage, completionOption, disposeMessage)
+        {
+        }
+
+        public override Task<Tuple<HttpResponseMessage, T>> SendAsync(CancellationToken cancellationToken)
+        {
+            return HttpClient.SendAsync(RequestMessage, CompletionOption, cancellationToken)
+                .Then(
+                    task =>
+                    {
+                        return DeserializeResultAsync(task.Result, cancellationToken)
+                            .Select(innerTask => Tuple.Create(task.Result, innerTask.Result));
+                    });
+        }
+
+        protected virtual Task<T> DeserializeResultAsync(HttpResponseMessage responseMessage, CancellationToken cancellationToken)
+        {
+            return responseMessage.Content.ReadAsStringAsync()
+                .Select(task => JsonConvert.DeserializeObject<T>(task.Result));
+        }
+    }
+}
