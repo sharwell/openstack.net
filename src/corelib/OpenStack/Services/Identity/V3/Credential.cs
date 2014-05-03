d502a6b2888029c2cad7b9ff470c7af204f43457
Sam Harwell <sam.harwell@rackspace.com>
2014-05-03 15:35:02 -0500

    Add the initial object model and API for Identity V3

---
+++ src/corelib/OpenStack/Services/Identity/V3/Credential.cs
@@ -0,0 +1,49 @@
+﻿namespace OpenStack.Services.Identity.V3
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+#if NET45PLUS
+    using System.Collections.ObjectModel;
+#else
+    using OpenStack.Collections;
+#endif
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class Credential : CredentialData
+    {
+        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        private CredentialId _id;
+
+        [JsonProperty("links", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        private Dictionary<string, string> _links;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Credential"/> class
+        /// during JSON deserialization.
+        /// </summary>
+        [JsonConstructor]
+        protected Credential()
+        {
+        }
+
+        public CredentialId Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public ReadOnlyDictionary<string, string> Links
+        {
+            get
+            {
+                if (_links == null)
+                    return null;
+
+                return new ReadOnlyDictionary<string, string>(_links);
+            }
+        }
+    }
+}
