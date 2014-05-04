d44c549bb8d8c3ed7407489e3e1f4e42625a9051
Sam Harwell <sam.harwell@rackspace.com>
2014-05-03 20:35:38 -0500

    Add IHttpApiCallFactory interface and default implementation HttpApiCallFactory

---
+++ src/corelib/OpenStack/Net/HttpApiCallFactory.cs
@@ -0,0 +1,59 @@
+﻿namespace OpenStack.Net
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Stream = System.IO.Stream;
+
+    public class HttpApiCallFactory : IHttpApiCallFactory
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpApiCallFactory()
+            : this(new HttpClient())
+        {
+        }
+
+        public HttpApiCallFactory(HttpClient httpClient)
+        {
+            if (httpClient == null)
+                throw new ArgumentNullException("httpClient");
+
+            _httpClient = httpClient;
+        }
+
+        protected HttpClient HttpClient
+        {
+            get
+            {
+                return _httpClient;
+            }
+        }
+
+        public virtual HttpApiCall<T> CreateJsonApiCall<T>(HttpRequestMessage requestMessage)
+        {
+            return new JsonHttpApiCall<T>(HttpClient, requestMessage, HttpCompletionOption.ResponseContentRead);
+        }
+
+        public virtual HttpApiCall<Stream> CreateStreamingApiCall(HttpRequestMessage requestMessage)
+        {
+            return new StreamingHttpApiCall(HttpClient, requestMessage);
+        }
+
+        public HttpApiCall CreateBasicApiCall(HttpRequestMessage requestMessage)
+        {
+            return CreateBasicApiCall(requestMessage, HttpCompletionOption.ResponseContentRead);
+        }
+
+        public virtual HttpApiCall CreateBasicApiCall(HttpRequestMessage requestMessage, HttpCompletionOption completionOption)
+        {
+            return new HttpApiCall(HttpClient, requestMessage, completionOption);
+        }
+
+        public HttpApiCall<T> CreateCustomApiCall<T>(HttpRequestMessage requestMessage, HttpCompletionOption completionOption, Func<HttpResponseMessage, CancellationToken, Task<T>> deserializeResult)
+        {
+            return new CustomHttpApiCall<T>(HttpClient, requestMessage, completionOption, deserializeResult);
+        }
+    }
+}
---
+++ src/corelib/OpenStack/Net/IHttpApiCallFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace OpenStack.Net
+{
+    using System;
+    using System.Threading.Tasks;
+    using CancellationToken = System.Threading.CancellationToken;
+    using HttpCompletionOption = System.Net.Http.HttpCompletionOption;
+    using HttpRequestMessage = System.Net.Http.HttpRequestMessage;
+    using HttpResponseMessage = System.Net.Http.HttpResponseMessage;
+    using Stream = System.IO.Stream;
+
+    public interface IHttpApiCallFactory
+    {
+        HttpApiCall<T> CreateJsonApiCall<T>(HttpRequestMessage requestMessage);
+
+        HttpApiCall<Stream> CreateStreamingApiCall(HttpRequestMessage requestMessage);
+
+        HttpApiCall CreateBasicApiCall(HttpRequestMessage requestMessage, HttpCompletionOption completionOption);
+
+        HttpApiCall<T> CreateCustomApiCall<T>(HttpRequestMessage requestMessage, HttpCompletionOption completionOption, Func<HttpResponseMessage, CancellationToken, Task<T>> deserializeResult);
+    }
+}
