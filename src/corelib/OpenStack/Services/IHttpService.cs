f6711eb54214b478e83e7247b9e107c5dddd989a
Sam Harwell <sam.harwell@rackspace.com>
2014-05-28 12:33:12 -0500

    Documentation updates

--- src/corelib/OpenStack/Services/IHttpService.cs
+++ src/corelib/OpenStack/Services/IHttpService.cs
@@ -7,12 +7,68 @@
     using System.Threading.Tasks;
     using Rackspace.Net;
 
+    /// <summary>
+    /// This is the base interface for a service API exposed over HTTP. It defines the
+    /// essential methods required for applications to write extension methods to support
+    /// arbitrary custom features of the service, as opposed to requiring applications
+    /// derived from an actual implementation.
+    /// </summary>
+    /// <preliminary/>
     public interface IHttpService
     {
+        /// <summary>
+        /// Creates a task continuation function responsible for creating an <see cref="HttpRequestMessage"/> for use
+        /// in asynchronous REST API calls. The input to the continuation function is a completed task which
+        /// provides the base absolute URI for the service for use in binding the URI templates for REST API calls,
+        /// and the <see cref="CancellationToken"/> which the task should observe.
+        /// </summary>
+        /// <typeparam name="T">The type used to represent replacement parameters for the URI Template expansion process.</typeparam>
+        /// <param name="method">The <see cref="HttpMethod"/> to use for the request.</param>
+        /// <param name="template">The <see cref="UriTemplate"/> for the target URI.</param>
+        /// <param name="parameters">A collection of parameters for binding the URI template in a call to <see cref="UriTemplate.BindByName{T}(Uri, IDictionary{string, T})"/>.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that the task will observe.</param>
+        /// <returns>A task continuation delegate which can be used to create an <see cref="HttpRequestMessage"/> following the completion of a task that obtains the base URI for a service.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="method"/> is <see langword="null"/>.
+        /// <para>-or-</para>
+        /// <para>If <paramref name="template"/> is <see langword="null"/>.</para>
+        /// <para>-or-</para>
+        /// <para>If <paramref name="parameters"/> is <see langword="null"/>.</para>
+        /// </exception>
         Func<Task<Uri>, Task<HttpRequestMessage>> PrepareRequestAsyncFunc<T>(HttpMethod method, UriTemplate template, IDictionary<string, T> parameters, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Creates a task continuation function responsible for creating an <see cref="HttpRequestMessage"/> for use
+        /// in asynchronous REST API calls. The input to the continuation function is a completed task which
+        /// provides the base absolute URI for the service for use in binding the URI templates for REST API calls,
+        /// and the <see cref="CancellationToken"/> which the task should observe.
+        /// </summary>
+        /// <typeparam name="T">The type used to represent replacement parameters for the URI Template expansion process.</typeparam>
+        /// <typeparam name="TBody">The type modeling the body of the request.</typeparam>
+        /// <param name="method">The <see cref="HttpMethod"/> to use for the request.</param>
+        /// <param name="template">The <see cref="UriTemplate"/> for the target URI.</param>
+        /// <param name="parameters">A collection of parameters for binding the URI template in a call to <see cref="UriTemplate.BindByName{T}(Uri, IDictionary{string, T})"/>.</param>
+        /// <param name="body">A object modeling the body of the request. The service client implementation serializes this object, often using JSON.NET, to the form used in the HTTP request body.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that the task will observe.</param>
+        /// <returns>A task continuation delegate which can be used to create an <see cref="HttpRequestMessage"/> following the completion of a task that obtains the base URI for a service.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="method"/> is <see langword="null"/>.
+        /// <para>-or-</para>
+        /// <para>If <paramref name="template"/> is <see langword="null"/>.</para>
+        /// <para>-or-</para>
+        /// <para>If <paramref name="parameters"/> is <see langword="null"/>.</para>
+        /// </exception>
         Func<Task<Uri>, Task<HttpRequestMessage>> PrepareRequestAsyncFunc<T, TBody>(HttpMethod method, UriTemplate template, IDictionary<string, T> parameters, TBody body, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Gets the base absolute URI to use for making asynchronous REST API calls to this service.
+        /// </summary>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that the task will observe.</param>
+        /// <returns>
+        /// A <see cref="Task"/> object representing the asynchronous operation. When the task
+        /// completes successfully, the <see cref="Task{TResult}.Result"/> property will contain
+        /// a <see cref="Uri"/> representing the base absolute URI for the service.
+        /// </returns>
         Task<Uri> GetBaseUriAsync(CancellationToken cancellationToken);
     }
 }
--- src/corelib/OpenStack/Services/ObjectStorage/V1/ObjectMetadata.cs
+++ src/corelib/OpenStack/Services/ObjectStorage/V1/ObjectMetadata.cs
@@ -1,17 +1,42 @@
 ﻿namespace OpenStack.Services.ObjectStorage.V1
 {
+    using System;
     using System.Collections.Generic;
     using System.Net.Http;
 
+    /// <summary>
+    /// This class extends <see cref="StorageMetadata"/> to represent metadata associated
+    /// with an object in the <see cref="IObjectStorageService"/>.
+    /// </summary>
     public class ObjectMetadata : StorageMetadata
     {
+        /// <summary>
+        /// The prefix for HTTP headers representing custom object metadata.
+        /// </summary>
         public static readonly string ObjectMetadataPrefix = "X-Object-Meta-";
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectMetadata"/> class from
+        /// the specified <see cref="HttpResponseMessage"/>.
+        /// </summary>
+        /// <param name="responseMessage">The <see cref="HttpResponseMessage"/> from which to extract the object metadata information.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="responseMessage"/> is <see langword="null"/>.</exception>
         public ObjectMetadata(HttpResponseMessage responseMessage)
             : base(responseMessage, ObjectMetadataPrefix)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ObjectMetadata"/> class with
+        /// the specified HTTP headers and custom metadata.
+        /// </summary>
+        /// <param name="headers">A collection of HTTP headers associated with the object.</param>
+        /// <param name="metadata">A collection of custom metadata associated with the object.</param>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="headers"/> is <see langword="null"/>.
+        /// <para>-or-</para>
+        /// <para>If<paramref name="metadata"/> is <see langword = "null" />.</para>
+        /// </exception>
         public ObjectMetadata(IDictionary<string, string> headers, IDictionary<string, string> metadata)
             : base(headers, metadata)
         {
