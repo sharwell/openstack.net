95077ab278f99f533e275ca7a5f8151392aa286a
Sam Harwell <sam.harwell@rackspace.com>
2014-05-29 15:56:51 -0500

    Add test to clarify behavior of UriExtensions.GetSegments

---
+++ src/testing/unit/UriExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿namespace OpenStackNet.Testing.Unit
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenStack.Compat;
+
+    [TestClass]
+    public class UriExtensionsTests
+    {
+        [TestMethod]
+        public void TestGetSegments()
+        {
+            Uri uri = new Uri("http://www.example.com/path/to/file");
+
+            string[] expectedSegments = { "/", "path/", "to/", "file" };
+            string[] referenceSegments = uri.Segments;
+            CollectionAssert.AreEqual(expectedSegments, referenceSegments);
+
+            string[] extensionSegments = uri.GetSegments();
+            CollectionAssert.AreEqual(referenceSegments, extensionSegments);
+        }
+    }
+}
