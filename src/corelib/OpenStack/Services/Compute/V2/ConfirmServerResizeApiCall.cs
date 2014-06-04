107114680f5c6160c6672c4a3eef1a10043969cd
Sam Harwell <sam.harwell@rackspace.com>
2014-06-04 10:57:22 -0500

    Fix inconsistent method and type names in Compute V2

--- src/corelib/OpenStack/Services/Compute/V2/ConfirmServerResizeApiCall.cs
+++ src/corelib/OpenStack/Services/Compute/V2/ConfirmServerResizeApiCall.cs
@@ -4,21 +4,21 @@ namespace OpenStack.Services.Compute.V2
     using OpenStack.Net;
 
     /// <summary>
-    /// This class represents a prepared API call to revert a server resize operation.
+    /// This class represents a prepared API call to confirm a server resize operation.
     /// </summary>
-    /// <seealso cref="IComputeService.PrepareRevertResizedServerAsync"/>
-    /// <seealso cref="ComputeServiceExtensions.RevertResizedServerAsync"/>
+    /// <seealso cref="IComputeService.PrepareConfirmServerResizeAsync"/>
+    /// <seealso cref="ComputeServiceExtensions.ConfirmServerResizeAsync"/>
     /// <threadsafety static="true" instance="false"/>
     /// <preliminary/>
-    public class RevertResizedServerApiCall : DelegatingHttpApiCall<string>
+    public class ConfirmServerResizeApiCall : DelegatingHttpApiCall<string>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="RevertResizedServerApiCall"/> class
+        /// Initializes a new instance of the <see cref="ConfirmServerResizeApiCall"/> class
         /// with the behavior provided by another <see cref="IHttpApiCall{T}"/> instance.
         /// </summary>
         /// <param name="httpApiCall">The <see cref="IHttpApiCall{T}"/> providing the behavior for the API call.</param>
         /// <exception cref="ArgumentNullException">If <paramref name="httpApiCall"/> is <see langword="null"/>.</exception>
-        public RevertResizedServerApiCall(IHttpApiCall<string> httpApiCall)
+        public ConfirmServerResizeApiCall(IHttpApiCall<string> httpApiCall)
             : base(httpApiCall)
         {
         }
--- src/corelib/OpenStack/Services/Compute/V2/RevertServerResizeApiCall.cs
+++ src/corelib/OpenStack/Services/Compute/V2/RevertServerResizeApiCall.cs
@@ -4,21 +4,21 @@ namespace OpenStack.Services.Compute.V2
     using OpenStack.Net;
 
     /// <summary>
-    /// This class represents a prepared API call to confirm a server resize operation.
+    /// This class represents a prepared API call to revert a server resize operation.
     /// </summary>
-    /// <seealso cref="IComputeService.PrepareConfirmServerResizedAsync"/>
-    /// <seealso cref="ComputeServiceExtensions.ConfirmServerResizedAsync"/>
+    /// <seealso cref="IComputeService.PrepareRevertServerResizeAsync"/>
+    /// <seealso cref="ComputeServiceExtensions.RevertServerResizeAsync"/>
     /// <threadsafety static="true" instance="false"/>
     /// <preliminary/>
-    public class ConfirmServerResizedApiCall : DelegatingHttpApiCall<string>
+    public class RevertServerResizeApiCall : DelegatingHttpApiCall<string>
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConfirmServerResizedApiCall"/> class
+        /// Initializes a new instance of the <see cref="RevertServerResizeApiCall"/> class
         /// with the behavior provided by another <see cref="IHttpApiCall{T}"/> instance.
         /// </summary>
         /// <param name="httpApiCall">The <see cref="IHttpApiCall{T}"/> providing the behavior for the API call.</param>
         /// <exception cref="ArgumentNullException">If <paramref name="httpApiCall"/> is <see langword="null"/>.</exception>
-        public ConfirmServerResizedApiCall(IHttpApiCall<string> httpApiCall)
+        public RevertServerResizeApiCall(IHttpApiCall<string> httpApiCall)
             : base(httpApiCall)
         {
         }
