dee639dc81b482b162d05f441f05256939896324
Sam Harwell <sam.harwell@rackspace.com>
2014-06-09 12:40:25 -0500

    Updated documentation for compute extensions object model

--- src/corelib/OpenStack/Services/Compute/V2/Extension.cs
+++ src/corelib/OpenStack/Services/Compute/V2/Extension.cs
@@ -1,30 +1,58 @@
 ﻿namespace OpenStack.Services.Compute.V2
 {
     using System;
+    using System.Collections.ObjectModel;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
     using OpenStack.ObjectModel;
+    using INetworkingService = OpenStack.Services.Networking.V2.INetworkingService;
 
+    /// <summary>
+    /// This class models the JSON representation of an extension resource in
+    /// the <see cref="IComputeService"/> or <see cref="INetworkingService"/>.
+    /// </summary>
+    /// <threadsafety static="true" instance="false"/>
+    /// <preliminary/>
     [JsonObject(MemberSerialization.OptIn)]
     public class Extension : ExtensibleJsonObject
     {
+#pragma warning disable 649 // Field 'fieldName' is never assigned to, and will always have its default value {value}
+        /// <summary>
+        /// This is the backing field for the <see cref="Alias"/> property.
+        /// </summary>
         [JsonProperty("alias", DefaultValueHandling = DefaultValueHandling.Ignore)]
         private ExtensionAlias _alias;
 
+        /// <summary>
+        /// This is the backing field for the <see cref="Name"/> property.
+        /// </summary>
         [JsonProperty("name", DefaultValueHandling = DefaultValueHandling.Ignore)]
         private string _name;
 
+        /// <summary>
+        /// This is the backing field for the <see cref="Description"/> property.
+        /// </summary>
         [JsonProperty("description", DefaultValueHandling = DefaultValueHandling.Ignore)]
         private string _description;
 
+        /// <summary>
+        /// This is the backing field for the <see cref="Namespace"/> property.
+        /// </summary>
         [JsonProperty("namespace", DefaultValueHandling = DefaultValueHandling.Ignore)]
         private string _namespace;
 
+        /// <summary>
+        /// This is the backing field for the <see cref="LastModified"/> property.
+        /// </summary>
         [JsonProperty("updated", DefaultValueHandling = DefaultValueHandling.Ignore)]
         private DateTimeOffset? _updated;
 
+        /// <summary>
+        /// This is the backing field for the <see cref="Links"/> property.
+        /// </summary>
         [JsonProperty("links", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        private JToken _links; // seriously, what is this?
+        private JToken[] _links; // seriously, what is this?
+#pragma warning restore 649
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Extension"/> class
@@ -35,6 +63,14 @@
         {
         }
 
+        /// <summary>
+        /// Gets the alias which serves as a unique identifier for the extension.
+        /// </summary>
+        /// <value>
+        /// The alias for the extension.
+        /// <para>-or-</para>
+        /// <para><see langword="null"/> if the JSON representation did not include the underlying property.</para>
+        /// </value>
         public ExtensionAlias Alias
         {
             get
@@ -43,6 +79,14 @@
             }
         }
 
+        /// <summary>
+        /// Gets the name of the extension.
+        /// </summary>
+        /// <value>
+        /// The name of the extension.
+        /// <para>-or-</para>
+        /// <para><see langword="null"/> if the JSON representation did not include the underlying property.</para>
+        /// </value>
         public string Name
         {
             get
@@ -51,6 +95,14 @@
             }
         }
 
+        /// <summary>
+        /// Gets a description of the extension.
+        /// </summary>
+        /// <value>
+        /// A description of the extension.
+        /// <para>-or-</para>
+        /// <para><see langword="null"/> if the JSON representation did not include the underlying property.</para>
+        /// </value>
         public string Description
         {
             get
@@ -67,6 +119,14 @@
             }
         }
 
+        /// <summary>
+        /// Gets a timestamp indicating when the extension resource was last modified.
+        /// </summary>
+        /// <value>
+        /// A timestamp indicating when the extension resource was last modified.
+        /// <para>-or-</para>
+        /// <para><see langword="null"/> if the JSON representation did not include the underlying property.</para>
+        /// </value>
         public DateTimeOffset? LastModified
         {
             get
@@ -75,11 +135,14 @@
             }
         }
 
-        public JToken Links
+        public ReadOnlyCollection<JToken> Links
         {
             get
             {
-                return _links;
+                if (_links == null)
+                    return null;
+
+                return new ReadOnlyCollection<JToken>(_links);
             }
         }
     }
--- src/corelib/OpenStack/Services/Compute/V2/GetExtensionApiCall.cs
+++ src/corelib/OpenStack/Services/Compute/V2/GetExtensionApiCall.cs
@@ -1,9 +1,25 @@
 ﻿namespace OpenStack.Services.Compute.V2
 {
+    using System;
     using OpenStack.Net;
+    using INetworkingService = OpenStack.Services.Networking.V2.INetworkingService;
 
+    /// <summary>
+    /// This class represents a prepared API call to get an extension resource in the
+    /// <see cref="IComputeService"/> or <see cref="INetworkingService"/>.
+    /// </summary>
+    /// <seealso cref="IComputeService.PrepareGetExtensionAsync"/>
+    /// <seealso cref="ComputeServiceExtensions.GetExtensionAsync"/>
+    /// <threadsafety static="true" instance="false"/>
+    /// <preliminary/>
     public class GetExtensionApiCall : DelegatingHttpApiCall<ExtensionResponse>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetExtensionApiCall"/> class
+        /// with the behavior provided by another <see cref="IHttpApiCall{T}"/> instance.
+        /// </summary>
+        /// <param name="httpApiCall">The <see cref="IHttpApiCall{T}"/> providing the behavior for the API call.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="httpApiCall"/> is <see langword="null"/>.</exception>
         public GetExtensionApiCall(IHttpApiCall<ExtensionResponse> httpApiCall)
             : base(httpApiCall)
         {
