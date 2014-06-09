d8dd4411421df9cdf4854b16d20541b393213e29
Sam Harwell <sam.harwell@rackspace.com>
2014-06-09 12:40:25 -0500

    Updated documentation for compute extensions object model

--- src/corelib/OpenStack/Services/Compute/V2/ExtensionResponse.cs
+++ src/corelib/OpenStack/Services/Compute/V2/ExtensionResponse.cs
@@ -3,11 +3,22 @@
     using Newtonsoft.Json;
     using OpenStack.ObjectModel;
 
+    /// <summary>
+    /// This class models the JSON representation used for the result of the API calls
+    /// which return an <seealso cref="V2.Extension"/> resource.
+    /// </summary>
+    /// <threadsafety static="true" instance="false"/>
+    /// <preliminary/>
     [JsonObject(MemberSerialization.OptIn)]
     public class ExtensionResponse : ExtensibleJsonObject
     {
+#pragma warning disable 649 // Field 'fieldName' is never assigned to, and will always have its default value {value}
+        /// <summary>
+        /// This is the backing field for the <see cref="Extension"/> property.
+        /// </summary>
         [JsonProperty("extension", DefaultValueHandling = DefaultValueHandling.Ignore)]
         private Extension _extension;
+#pragma warning restore 649
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ExtensionResponse"/> class
@@ -18,6 +29,14 @@
         {
         }
 
+        /// <summary>
+        /// Gets an <see cref="V2.Extension"/> object describing the API extension.
+        /// </summary>
+        /// <value>
+        /// An <see cref="V2.Extension"/> object describing the API extension.
+        /// <para>-or-</para>
+        /// <para><see langword="null"/> if the JSON representation did not include the underlying property.</para>
+        /// </value>
         public Extension Extension
         {
             get
--- src/corelib/OpenStack/Services/Compute/V2/ListExtensionsApiCall.cs
+++ src/corelib/OpenStack/Services/Compute/V2/ListExtensionsApiCall.cs
@@ -1,10 +1,26 @@
 ﻿namespace OpenStack.Services.Compute.V2
 {
+    using System;
     using OpenStack.Collections;
     using OpenStack.Net;
+    using INetworkingService = OpenStack.Services.Networking.V2.INetworkingService;
 
+    /// <summary>
+    /// This class represents a prepared API call to list the extension resources in the
+    /// <see cref="IComputeService"/> or <see cref="INetworkingService"/>.
+    /// </summary>
+    /// <seealso cref="IComputeService.PrepareListExtensionsAsync"/>
+    /// <seealso cref="ComputeServiceExtensions.ListExtensionsAsync"/>
+    /// <threadsafety static="true" instance="false"/>
+    /// <preliminary/>
     public class ListExtensionsApiCall : DelegatingHttpApiCall<ReadOnlyCollectionPage<Extension>>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListExtensionsApiCall"/> class
+        /// with the behavior provided by another <see cref="IHttpApiCall{T}"/> instance.
+        /// </summary>
+        /// <param name="httpApiCall">The <see cref="IHttpApiCall{T}"/> providing the behavior for the API call.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="httpApiCall"/> is <see langword="null"/>.</exception>
         public ListExtensionsApiCall(IHttpApiCall<ReadOnlyCollectionPage<Extension>> httpApiCall)
             : base(httpApiCall)
         {
