b080bf66dd7f018f84f85380dc1f4d342bd83539
Sam Harwell <sam.harwell@rackspace.com>
2014-06-23 14:27:24 -0500

    Update Message<T> and QueuedMessage to more closely follow current object model practices

--- src/corelib/OpenStack/Services/Queues/V1/QueuedMessage.cs
+++ src/corelib/OpenStack/Services/Queues/V1/QueuedMessage.cs
@@ -12,34 +12,21 @@
     /// <threadsafety static="true" instance="false"/>
     /// <preliminary/>
     [JsonObject(MemberSerialization.OptIn)]
-    public class QueuedMessage
+    public class QueuedMessage : Message<JObject>
     {
 #pragma warning disable 649 // Field 'fieldName' is never assigned to, and will always have its default value {value}
         /// <summary>
         /// The backing field for the <see cref="Href"/> property.
         /// </summary>
-        [JsonProperty("href")]
-        private Uri _href;
-
-        /// <summary>
-        /// The backing field for the <see cref="TimeToLive"/> property.
-        /// The value is stored in seconds.
-        /// </summary>
-        [JsonProperty("ttl")]
-        private long _ttl;
+        [JsonProperty("href", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        private string _href;
 
         /// <summary>
         /// The backing field for the <see cref="Age"/> property.
         /// The value is stored in seconds.
         /// </summary>
-        [JsonProperty("age")]
-        private long _age;
-
-        /// <summary>
-        /// The backing field for the <see cref="Body"/> property.
-        /// </summary>
-        [JsonProperty("body")]
-        private JObject _body;
+        [JsonProperty("age", DefaultValueHandling = DefaultValueHandling.Ignore)]
+        private long? _age;
 #pragma warning restore 649
 
         /// <summary>
@@ -58,10 +45,10 @@
         {
             get
             {
-                if (Href == null)
+                Uri href = Href;
+                if (href == null)
                     return null;
 
-                Uri href = Href;
                 // make sure we have an absolute URI, or Segments will throw an InvalidOperationException
                 if (!href.IsAbsoluteUri)
                     href = new Uri(new Uri("http://example.com"), href);
@@ -81,40 +68,24 @@
         {
             get
             {
-                return _href;
-            }
-        }
+                if (_href == null)
+                    return null;
 
-        /// <summary>
-        /// Gets the time-to-live of the message.
-        /// </summary>
-        public TimeSpan TimeToLive
-        {
-            get
-            {
-                return TimeSpan.FromSeconds(_ttl);
+                return new Uri(_href, UriKind.RelativeOrAbsolute);
             }
         }
 
         /// <summary>
         /// Gets the age of the message.
         /// </summary>
-        public TimeSpan Age
+        public TimeSpan? Age
         {
             get
             {
-                return TimeSpan.FromSeconds(_age);
-            }
-        }
+                if (_age == null)
+                    return null;
 
-        /// <summary>
-        /// Gets the JSON body of the message as a dynamic <see cref="JObject"/>.
-        /// </summary>
-        public JObject Body
-        {
-            get
-            {
-                return _body;
+                return TimeSpan.FromSeconds(_age.Value);
             }
         }
     }
